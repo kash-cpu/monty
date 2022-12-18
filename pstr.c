851307dffb262d799dc7bd36df885c24c52ce52f
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-12-17 22:51:12 -0800

    correction and changes

--- pstr.c
+++ pstr.c
@@ -1,25 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "monty.h"
-/**
- * f_pstr - prints the string starting at the top of the stack,
- * followed by a new
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_pstr(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
 
-	h = *head;
-	while (h)
+/**
+ * pstr - prints the contents of a stack_t stack as a string
+ * @stack: stack given by main
+ * @line_cnt: line counter for error messages
+ *
+ * Return: nothing
+ */
+void pstr(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+{
+	stack_t *current = *stack;
+
+	while (current)
 	{
-		if (h->n > 127 || h->n <= 0)
-		{
+		if (current->n <= 0 || current->n > 127)
 			break;
-		}
-		printf("%c", h->n);
-		h = h->next;
+		putchar((char) current->n);
+		current = current->next;
 	}
-	printf("\n");
+	putchar('\n');
 }
+
